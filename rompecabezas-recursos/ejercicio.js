676e992d71154c2e92e5fa9da0f90926c16e58c7
derpaherper1 <juan.mrt159@gmail.com>
2018-08-14 20:00:08 -0300

    hola

---
+++ rompecabezas-recursos/ejercicio.js
@@ -0,0 +1,9 @@
+var ingresarProducto;
+do {
+ ingresarProducto=  parseInt(prompt("1 Leche" \n +
+  "2 tomate" \n +
+  "3 cebolla" \n +
+  "4 papas" \n +
+  "5 manzanas"\n
+  "6 salir"\n))
+} while (ingresarProducto !==6);
